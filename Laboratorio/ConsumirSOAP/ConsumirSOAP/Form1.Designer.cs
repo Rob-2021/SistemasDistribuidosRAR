5812e733d1303d8545563d42bda64dd0a5f2f18d
Rob-2021 <dragrober.rar>
2022-06-20 15:55:23 -0400

    20-6-22

---
+++ Laboratorio/ConsumirSOAP/ConsumirSOAP/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ConsumirSOAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstViewDatos = new System.Windows.Forms.ListView();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstViewDatos
+            // 
+            this.lstViewDatos.HideSelection = false;
+            this.lstViewDatos.Location = new System.Drawing.Point(62, 92);
+            this.lstViewDatos.Name = "lstViewDatos";
+            this.lstViewDatos.Size = new System.Drawing.Size(366, 97);
+            this.lstViewDatos.TabIndex = 16;
+            this.lstViewDatos.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(272, 34);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 15;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "CI:";
+            // 
+            // txtCi
+            // 
+            this.txtCi.Location = new System.Drawing.Point(136, 36);
+            this.txtCi.Name = "txtCi";
+            this.txtCi.Size = new System.Drawing.Size(100, 20);
+            this.txtCi.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 239);
+            this.Controls.Add(this.lstViewDatos);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstViewDatos;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCi;
+    }
+}
+
---
+++ Laboratorio/ConsumirSOAP/ConsumirSOAP/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsumirSOAP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            SERECI.WebService1SoapClient sw = new SERECI.WebService1SoapClient();
+            SERECI.Persona p = sw.ObtenerDatos(txtCi.Text);
+            string carnet = txtCi.Text.ToString();
+
+            if (p != null)
+            {
+                lstViewDatos.Items.Add(p.carnet);
+                lstViewDatos.Items.Add(p.nombre);
+                lstViewDatos.Items.Add(p.primerApellido);
+                lstViewDatos.Items.Add(p.segundoApellido);
+                lstViewDatos.Items.Add(p.fechaNacimiento);
+                lstViewDatos.Items.Add(p.sexo);
+                lstViewDatos.Items.Add(p.estadoCivil);
+            }
+
+
+            txtCi.Text = "";
+            txtCi.Focus();
+
+            //cl.ObtenerDatos(carnet);
+
+            //lstViewDatos.Items.Add(cl.ObtenerDatos(carnet));
+        }
+    }
+    
+}
